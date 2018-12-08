b84c58102d6acb601db7b5fbc981dd11c347cccf
Leo Canela <leocanela@Leos-MacBook-Air.local>
2018-12-08 18:10:36 -0200

    Updates

---
+++ precache-manifest.6be12ad2fe152bf7fa0cd644821ad948.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "945d1f78f6091e76a393",
+    "url": "/static/css/main.a0548cc5.chunk.css"
+  },
+  {
+    "revision": "945d1f78f6091e76a393",
+    "url": "/static/js/main.945d1f78.chunk.js"
+  },
+  {
+    "revision": "e0676e15b5cd74b14eae",
+    "url": "/static/js/1.e0676e15.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "18a3c80e0a57f7f378f48aa7b05b547c",
+    "url": "/index.html"
+  }
+];
