d194fae0b47fe513a836b61b00f3b9ad92628e1b
Leo Canela <leocanela@Leos-MacBook-Air.local>
2018-12-08 18:12:33 -0200

    Updates

---
+++ precache-manifest.e03c657f65ec34be1ab224dba6dbd871.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "59e36d2fc8fc2f3d7740",
+    "url": "/leafCiap/static/css/main.a0548cc5.chunk.css"
+  },
+  {
+    "revision": "59e36d2fc8fc2f3d7740",
+    "url": "/leafCiap/static/js/main.59e36d2f.chunk.js"
+  },
+  {
+    "revision": "e0676e15b5cd74b14eae",
+    "url": "/leafCiap/static/js/1.e0676e15.chunk.js"
+  },
+  {
+    "revision": "89122e9563640ec61f85",
+    "url": "/leafCiap/static/js/runtime~main.89122e95.js"
+  },
+  {
+    "revision": "393357dceb0814b6ab2a8089b029e803",
+    "url": "/leafCiap/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6be12ad2fe152bf7fa0cd644821ad948.js"
+  "/leafCiap/precache-manifest.e03c657f65ec34be1ab224dba6dbd871.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/leafCiap/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
